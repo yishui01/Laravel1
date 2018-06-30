d6857f354b4336f024c1c908dddcecda2c282de2
zuiqiangqishao <727040917@qq.com>
2018-06-30 10:07:58 +0800

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -9,5 +9,30 @@ class UsersController extends Controller
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+
+    /**存储数据
+     * @param Request $request
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use http\Env\Request;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -27,4 +28,25 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**生成全球通用的头像
+     * @param string $size
+     * @return string
+     */
+    public function gravatar($size = '100')
+    {
+        /*
+        为 gravatar 方法传递的参数 size 指定了默认值 100；
+        通过 $this->attributes['email'] 获取到用户的邮箱；
+        使用 trim 方法剔除邮箱的前后空白内容；
+        用 strtolower 方法将邮箱转换为小写；
+        将小写的邮箱使用 md5 方法进行转码；
+        将转码后的邮箱与链接、尺寸拼接成完整的 URL 并返回；
+        */
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
+
+
 }
